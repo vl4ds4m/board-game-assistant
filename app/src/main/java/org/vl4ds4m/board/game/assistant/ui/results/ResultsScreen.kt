cedf60ba7f35b0ef48ad17c72336960c000fde33
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-04-14 19:33:56 +0300

    Update results screen

--- app/src/main/java/org/vl4ds4m/board/game/assistant/ui/results/ResultsScreen.kt
+++ app/src/main/java/org/vl4ds4m/board/game/assistant/ui/results/ResultsScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,11 +17,13 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.vl4ds4m.board.game.assistant.defaultGames
 import org.vl4ds4m.board.game.assistant.game.data.GameSessionInfo
+import org.vl4ds4m.board.game.assistant.ui.component.GameSessionCard
 import org.vl4ds4m.board.game.assistant.ui.theme.BoardGameAssistantTheme
 
 @Composable
@@ -42,37 +47,60 @@ fun ResultsScreenContent(
 ) {
     Column(
         modifier = modifier.padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(24.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
-            text = "Completed games",
-            modifier = Modifier.padding(8.dp),
-            style = MaterialTheme.typography.headlineSmall
+            text = "Game results",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.headlineMedium
         )
-        LazyColumn(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-        ) {
-            sessions.value.sortedByDescending { it.stopTime }
-                .onEachIndexed { index, session ->
-                    item(session.id) {
-                        Text(
-                            text = "${index + 1}. ${session.name}",
-                            modifier = Modifier
-                                .padding(vertical = 16.dp)
-                                .clickable { clickSession(session.id) },
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                    }
+        HorizontalDivider()
+        if (sessions.value.isEmpty()) {
+            Text(
+                text = "No completed games",
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .wrapContentSize()
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                itemsIndexed(
+                    items = sessions.value.sortedByDescending { it.stopTime },
+                    key = { _, s -> s.id }
+                ) { index, session ->
+                    GameSessionCard(
+                        text = "${index + 1}. ${session.name}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { clickSession(session.id) }
+                    )
                 }
+            }
         }
     }
 }
 
 @Preview
 @Composable
-private fun ResultsScreenPreview() {
+private fun SomeResultsScreenPreview() {
+    ResultsScreenPreview(sessionsInfo)
+}
+
+@Preview
+@Composable
+private fun EmptyResultsScreenPreview() {
+    ResultsScreenPreview(listOf())
+}
+
+@Composable
+private fun ResultsScreenPreview(sessionsInfo: List<GameSessionInfo>) {
     BoardGameAssistantTheme {
         ResultsScreenContent(
             sessions = remember { mutableStateOf(sessionsInfo) },
@@ -82,11 +110,10 @@ private fun ResultsScreenPreview() {
     }
 }
 
-private val sessionsInfo: List<GameSessionInfo> = defaultGames.filter { it.completed }
-    .mapIndexed { i, s ->
+private val sessionsInfo: List<GameSessionInfo> get() = defaultGames.mapIndexed { i, s ->
         GameSessionInfo(
             id = i.inc().toString(),
-            completed = s.completed,
+            completed = true,
             type = s.type,
             name = s.name,
             startTime = s.startTime,

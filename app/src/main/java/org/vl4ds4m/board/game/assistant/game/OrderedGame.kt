7e7e709ede76166e80c227f3b5700d229a3be38d
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-05-13 07:37:49 +0300

    Add description of base game interfaces

--- app/src/main/java/org/vl4ds4m/board/game/assistant/game/OrderedGame.kt
+++ app/src/main/java/org/vl4ds4m/board/game/assistant/game/OrderedGame.kt
@@ -2,10 +2,25 @@ package org.vl4ds4m.board.game.assistant.game
 
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * Abstraction for the game, where players have an order how they should make actions.
+ */
 interface OrderedGame : Game {
+    /**
+     * State flow of the id of the next player.
+     */
     val nextPid: StateFlow<PID?>
 
+    /**
+     * Select the next player.
+     * @see nextPid
+     */
     fun changeCurrentPid()
 
+    /**
+     * Change the order of the player.
+     * @param id Player id
+     * @param order New position of the player in the list of order.
+     */
     fun changePlayerOrder(id: PID, order: Int)
 }
